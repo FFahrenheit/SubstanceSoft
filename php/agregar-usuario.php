281f8126e02c64f4db9b5aba3e06314842570588
FFahrenheit <diskman199@gmail.com>
2019-02-08 00:16:23 -0600

    Avance
    Some code...

---
+++ php/agregar-usuario.php
@@ -0,0 +1,22 @@
+<?php
+    $nom = $_POST['nom'];
+    $pas = $_POST['pas'];
+    $usu = $_POST['usu'];
+    $pat = $_POST['pat'];
+    $mat = $_POST['mat'];
+    $dir = $_POST['dir'];
+    $tel = $_POST['tel'];
+    $tip = $_POST['tip'];
+
+    $connection = mysqli_connect("localhost", "root", "", "substancesoft") or die;
+
+    $query = "INSERT INTO usuario (username, password, nombre, apellido_p, apellido_m,
+    telefono, direccion, tipo) VALUES ('$usu', '$pas', '$nom', '$pat', '$mat', $tel, '$dir', '$tip')";
+
+    echo json_encode($query);
+
+    $result = mysqli_query($connection, $query);
+
+    echo json_encode("nice!");
+    mysqli_close($connection);
+?>
