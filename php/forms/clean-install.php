4275d9937f91801347f55fc95c20200082e8bff1
XV02 <lghhssi@gmail.com>
2019-02-19 12:25:48 -0600

    Sin Project Libre

--- php/forms/clean-install.php
+++ php/forms/clean-install.php
@@ -14,12 +14,12 @@
 
     $conexion = mysqli_connect("localhost", "root", "", "substancesoft") or die('"No se pudo conectar a la base de datos"');
 
-    $query = "INSERT INTO usuario (username, password, nombre, apellido_p, apellido_m, 
+    $query = "INSERT INTO usuario (username, password, nombre, apellido_p, apellido_m,
     telefono, direccion, tipo) VALUES ('$usu', '$pas', '$nom', '$pat', '$mat', $tel, '$dir', 'administrador')";
 
     $result = mysqli_query($conexion, $query) or die('"El nombre de usuario ya existe"');
 
-    echo '"Exito!"';
+    echo json_encode("Exito!");
 
     mysqli_close($connection);
 ?>
