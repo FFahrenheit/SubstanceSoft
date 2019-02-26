b419f3c856a6100c267b0fb05174cb4c99fd5d58
XV02 <lghhssi@gmail.com>
2019-02-26 13:45:32 -0600

    Dependencia de internet
    Se eliminó la dependencia de internet tanto del framework como de los resources.

--- views/lists/listar-usuarios.php
+++ views/lists/listar-usuarios.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, inicial-scale=1">
-        
+
         <title>
             Mostrar usuarios
         </title>
@@ -32,7 +32,7 @@
                         <?php
                             $connection = mysqli_connect("localhost", "root", "", "substancesoft") or die;
 
-                            $query = "select * from usuario"; 
+                            $query = "select * from usuario";
 
                             $sql = mysqli_query($connection, $query) or die("error");
 
@@ -59,7 +59,7 @@
         </section>
     </body>
     <script src="js/agregar-usuario.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <script src="../../js/vendor/jquery-3.3.1.slim.min.js"></script>
+    <script src="../../js/vendor/popper.min.js"></script>
+    <script src="../../js/vendor/bootstrap.min.js"></script>
 </html>
