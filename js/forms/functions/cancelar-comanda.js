20216b19d9349168be27e31b4334105683ba85e5
Iván L. Murillo <diskman199@gmail.com>
2019-03-10 21:26:14 -0600

    Cancelación de comandas
    La cancelación de comandas está lista. Tal vez debería agregarse soporte de cancelar comandas que ya están listas.

---
+++ js/forms/functions/cancelar-comanda.js
@@ -0,0 +1,31 @@
+var user;
+
+function showDetails(button)
+{
+    user = button.id;
+    console.log(user);
+}
+
+function confirmDelete()
+{
+    var datos = new FormData();
+
+    datos.append("user",user);
+
+    console.log(user);
+    fetch('../../../php/forms/functions/cancelar-comanda.php', 
+    {
+        method: 'POST',
+        body: datos
+    })
+    .then (res => res.json())
+    .then (data =>
+    {
+        console.log("aqui llega js");
+        console.log(data);
+        alert(data);
+        window.location.reload(false); 
+
+    })
+}
+
---
+++ views/functions/forms/cancelacion.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, inicial-scale=1">
+
+        <title>
+            Cancelar comanda
+        </title>
+
+        <link href="../../../css/bs/bootstrap.min.css" rel="stylesheet">
+        <link href="../../../css/clean-install.css" rel="stylesheet">
+    </head>
+    <body>
+        <section class="container">
+            <h1 class="text-uppercase text-center">Cancelar comanda</h2>
+            <p class="lead text-center">Aquí puede cancelar comandas. Solo puede realizar cancelaciones en órdenes no listas.</p>
+            <div class="row">
+                <div class="col-sm">
+                <?php
+                    $clave  = $_GET['clave'];
+
+                    $connection = mysqli_connect("localhost", "root", "", "substancesoft") or die ("error en BD");
+
+                    $query = "select platillo.nombre as name, pedidos.clave as pk, platillo.precio as price, pedidos.estado as status
+                    from platillo, pedidos where pedidos.orden=$clave and platillo.clave = pedidos.platillo";
+
+                    $sql = mysqli_query($connection, $query) or die("error en query");
+
+                    echo "Comandas para la orden $clave";
+                ?>                    
+                    <div class="containter-fluid">
+                        <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <td>Platillo</td>
+                                <td>Precio</td>
+                                <td>Estado</td>
+                                <td> &nbsp;</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                if($sql->num_rows==0)
+                                {
+                                    echo "No hay comandas en la orden";
+                                }
+                                while($row = mysqli_fetch_array($sql))
+                                {
+                                ?>
+                            <tr>
+                                <td><?php echo $row['name'];?></td>
+                                <td><?php echo $row['price'];?></td>
+                                <td><?php echo $row['status'];?></td>
+                                <td>
+                                    <?php 
+                                        if($row['status']=='pedido')
+                                        {
+                                            echo '<a style="color: white;" data-toggle="modal" data-target="#delete"
+                                            class="btn btn-danger" id="'.$row['pk'].'" onClick="showDetails(this)">
+                                                Cancelar
+                                            </a>';
+                                        }
+                                    ?>
+                                </td>
+                            </tr>
+                            <?php
+                                }
+                            ?>
+                        </tbody>
+                        </table>
+                       <div  class="text-center"> 
+                        <a href="../modulo1-funcion2.php">
+                        <button type="button" class="btn btn-success">Regresar</button>
+                        </a>
+                        </div>
+                        <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Eliminar registro</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                ¿Seguro que desea cancelar la comanda?
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                <button type="button" class="btn btn-danger" onClick="confirmDelete()">Eliminar</button>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+    <script src="../../../js/forms/functions/cancelar-comanda.js"></script>
+    <script src="../../../js/vendor/jquery-3.3.1.slim.min.js"></script>
+    <script src="../../../js/vendor/popper.min.js"></script>
+    <script src="../../../js/vendor/bootstrap.min.js"></script>
+</html>
