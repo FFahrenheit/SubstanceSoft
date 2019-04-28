b6cba2c5fd475e3328d69ff63caebf149a4db6d9
Iván L. Murillo <diskman199@gmail.com>
2019-04-27 23:02:36 -0500

    Opciones limitadas electron
    Ya hay control sobre el menu app

--- SubstanceSoft/main.js
+++ SubstanceSoft/main.js
@@ -1,5 +1,5 @@
 const electron  = require('electron');
-const {app, BrowserWindow, globalShorcut} = electron;
+const {app, BrowserWindow, globalShorcut, Menu} = electron;
 var path = require('path')
 
 let mainWindow;
@@ -10,9 +10,11 @@ app.on('ready', () =>{
             titleBarStyle: 'hidden',
             minWidth: 1024,
             minHeight: 768,
+            //frame: false,
             show: false,
             backgroundColor: '#bababa',
-            icon: path.join(__dirname, 'images/64x64.png')
+            icon: path.join(__dirname, 'images/64x64.png'),
+            title: 'SubstanceSoft',
         }
     );
     mainWindow.setTitle('SubstanceSoft');
@@ -23,4 +25,92 @@ app.on('ready', () =>{
     {
         mainWindow = null;
     })
+
+    const template = [
+        {
+          label: 'Edit',
+          submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            { role: 'pasteandmatchstyle' },
+            { role: 'delete' },
+            { role: 'selectall' }
+          ]
+        },
+        {
+          label: 'View',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            //{ role: 'toggledevtools' },
+            { type: 'separator' },
+            { role: 'resetzoom' },
+            { role: 'zoomin' },
+            { role: 'zoomout' },
+            { type: 'separator' },
+            { role: 'togglefullscreen' }
+          ]
+        },
+        {
+          role: 'window',
+          submenu: [
+            { role: 'minimize' },
+            { role: 'close' }
+          ]
+        },
+        {
+          role: 'help',
+          submenu: [
+            {
+              label: 'Learn More',
+              click () { require('electron').shell.openExternal('https://github.com/FFahrenheit/SubstanceSoft') }
+            }
+          ]
+        }
+      ]
+      
+      if (process.platform === 'darwin') {
+        template.unshift({
+          label: app.getName(),
+          submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services' },
+            { type: 'separator' },
+            { role: 'hide' },
+            { role: 'hideothers' },
+            { role: 'unhide' },
+            { type: 'separator' },
+            { role: 'quit' }
+          ]
+        })
+      
+        // Edit menu
+        template[1].submenu.push(
+          { type: 'separator' },
+          {
+            label: 'Speech',
+            submenu: [
+              { role: 'startspeaking' },
+              { role: 'stopspeaking' }
+            ]
+          }
+        )
+      
+        // Window menu
+        template[3].submenu = [
+          { role: 'close' },
+          { role: 'minimize' },
+          { role: 'zoom' },
+          { type: 'separator' },
+          { role: 'front' }
+        ]
+      }
+
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
 });
