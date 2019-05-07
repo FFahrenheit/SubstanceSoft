58eca78481c546f03700d0d6361b91b56da45e78
Iván L. Murillo <diskman199@gmail.com>
2019-05-06 19:15:26 -0500

    Eliminar proveedor elimina todos los eventos creados

--- php/forms/eliminar-proveedor.php
+++ php/forms/eliminar-proveedor.php
@@ -1,7 +1,27 @@
 <?php
-    $clave = $_POST['clave'];
+    $clave = $_GET['clave'];
 
     $connection = mysqli_connect("localhost", "root", "", "substancesoft") or die('"No se ha conectar"');
+
+    $query = "SELECT nombre_trigger AS nombre FROM surtidos WHERE proveedor = $clave";
+
+    $result = mysqli_query($connection, $query) or die("'No hay triggers'");
+
+    $triggers = array();
+
+    $rows = $result->num_rows;
+
+    for($i = 0; $i< $rows; $i++)
+    {
+        $row = mysqli_fetch_array($result);
+        $triggers[$i] = $row['nombre'];
+    }
+
+    for($i = 0; $i < $rows; $i++)
+    {
+        $query = "DROP EVENT ".$triggers[$i];
+        $result = mysqli_query($connection,$query) or die("'No se pudo eliminar'");
+    }
 
     $query = "DELETE FROM proveedor WHERE clave = '$clave'";
 
