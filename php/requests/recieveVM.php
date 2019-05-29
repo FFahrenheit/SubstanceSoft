8e7916ef14f635d7abcdcc9c1e856e29079e2b41
XV02 <lghhssi@gmail.com>
2019-05-28 20:47:59 -0500

    Hum

--- php/requests/recieveVM.php
+++ php/requests/recieveVM.php
@@ -1,4 +1,5 @@
 <?php
+  exec('md %userprofile%\Documents\SubstanceSoft_Backup');
   $file = addslashes(file_get_contents($_FILES['sql']['tmp_name']));
   $fp = fopen('C:/users/%userprofile%/Documents/SubstanceSoft_Backup/backup.sql', 'w');
   fwrite($fp, $file);
